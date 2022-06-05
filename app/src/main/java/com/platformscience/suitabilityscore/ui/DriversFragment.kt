566dd032ff3a136e8a28b4a2c208eb97a7e9a192
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-06-05 15:03:10 -0700

    Add loading animation

--- app/src/main/java/com/platformscience/suitabilityscore/ui/DriversFragment.kt
+++ app/src/main/java/com/platformscience/suitabilityscore/ui/DriversFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.platformscience.suitabilityscore.databinding.FragmentDriversBinding
@@ -29,12 +32,33 @@ class DriversFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		
+		animateLoading()
+		
 		binding.rvDrivers.adapter = DriversAdapter()
 		binding.rvDrivers.setHasFixedSize(true)
 		
 		viewModel.drivers.observe(viewLifecycleOwner) {
+			binding.ivLoading.visibility = View.GONE
+			
 			val adapter = binding.rvDrivers.adapter as DriversAdapter
 			adapter.submitList(it)
 		}
+	}
+	
+	private fun animateLoading() {
+		val anim = RotateAnimation(
+			0.0f,
+			360.0f,
+			Animation.RELATIVE_TO_SELF,
+			0.5f,
+			Animation.RELATIVE_TO_SELF,
+			0.5f
+		)
+		
+		anim.interpolator = LinearInterpolator()
+		anim.repeatCount = Animation.INFINITE
+		anim.duration = 250
+		
+		binding.ivLoading.startAnimation(anim)
 	}
 }

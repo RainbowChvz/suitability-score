bab66b0c29d24e81f8dd76ceb3576cfeaf172d56
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-06-05 10:52:29 -0700

    Add DriversFragment and navigation controller

---
+++ app/src/main/java/com/platformscience/suitabilityscore/ui/DriversFragment.kt
@@ -0,0 +1,26 @@
+package com.platformscience.suitabilityscore.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.platformscience.suitabilityscore.databinding.FragmentDriversBinding
+
+class DriversFragment : Fragment() {
+	
+	private lateinit var binding: FragmentDriversBinding
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		
+		binding = FragmentDriversBinding.inflate(
+			inflater,
+			container,
+			false
+		)
+		return binding.root
+	}
+}

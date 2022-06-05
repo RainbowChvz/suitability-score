ef45c8660c7f9f24cc2180ee3af5b92298fb3ab4
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-06-05 12:26:39 -0700

    Add repository class and local source class

---
+++ app/src/main/java/com/platformscience/suitabilityscore/data/RepositoryData.kt
@@ -0,0 +1,51 @@
+package com.platformscience.suitabilityscore.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.platformscience.suitabilityscore.data.model.Driver
+import com.platformscience.suitabilityscore.data.model.Shipment
+import com.platformscience.suitabilityscore.data.source.LocalData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class RepositoryData {
+	
+	private val _drivers = MutableLiveData<List<Driver>>()
+	val drivers: LiveData<List<Driver>> = _drivers
+	
+	private val _shipments = MutableLiveData<List<Shipment>>()
+	val shipments: LiveData<List<Shipment>> = _shipments
+	
+	suspend fun getData() {
+		withContext(Dispatchers.IO) {
+			LocalData.getDataFromFile()?.apply {
+				_drivers.value = drivers.asDrivers()
+				_shipments.value = shipments.asShipments()
+			}
+		}
+	}
+}
+
+private fun List<String>.asDrivers(): List<Driver> {
+	
+	val list = mutableListOf<Driver>()
+	for (i in this) {
+		list.add(
+			Driver(i)
+		)
+	}
+	
+	return list
+}
+
+private fun List<String>.asShipments(): List<Shipment> {
+	
+	val list = mutableListOf<Shipment>()
+	for (i in this) {
+		list.add(
+			Shipment(i)
+		)
+	}
+	
+	return list
+}
---
+++ app/src/main/java/com/platformscience/suitabilityscore/data/source/LocalData.kt
@@ -0,0 +1,50 @@
+package com.platformscience.suitabilityscore.data.source
+
+import com.platformscience.suitabilityscore.data.model.ResponseData
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+object LocalData {
+	
+	private val jsonLocalData = "{\n" +
+			"  \"shipments\": [\n" +
+			"    \"215 Osinski Manors\",\n" +
+			"    \"9856 Marvin Stravenue\",\n" +
+			"    \"7127 Kathlyn Ferry\",\n" +
+			"    \"987 Champlin Lake\",\n" +
+			"    \"63187 Volkman Garden Suite 447\",\n" +
+			"    \"75855 Dessie Lights\",\n" +
+			"    \"1797 Adolf Island Apt. 744\",\n" +
+			"    \"2431 Lindgren Corners\",\n" +
+			"    \"8725 Aufderhar River Suite 859\",\n" +
+			"    \"79035 Shanna Light Apt. 322\"\n" +
+			"  ],\n" +
+			"  \"drivers\": [\n" +
+			"    \"Everardo Welch\",\n" +
+			"    \"Orval Mayert\",\n" +
+			"    \"Howard Emmerich\",\n" +
+			"    \"Izaiah Lowe\",\n" +
+			"    \"Monica Hermann\",\n" +
+			"    \"Ellis Wisozk\",\n" +
+			"    \"Noemie Murphy\",\n" +
+			"    \"Cleve Durgan\",\n" +
+			"    \"Murphy Mosciski\",\n" +
+			"    \"Kaiser Sose\"\n" +
+			"  ]\n" +
+			"}"
+	
+	fun getDataFromFile(): ResponseData? {
+
+//		val jsonLocalData = resources.openRawResource(R.raw.data).bufferedReader().use {
+//			it.readText()
+//		}
+		
+		val moshi = Moshi.Builder()
+			.add(KotlinJsonAdapterFactory())
+			.build()
+		val jsonAdapter: JsonAdapter<ResponseData> = moshi.adapter(ResponseData::class.java)
+		
+		return jsonAdapter.fromJson(jsonLocalData)
+	}
+}

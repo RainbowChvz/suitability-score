1cea07f694132dd5484263a167a097c5e9717476
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-06-05 10:53:01 -0700

    Fix warning "onCreateView always returns non-null type"

--- app/src/main/java/com/platformscience/suitabilityscore/ui/DriversFragment.kt
+++ app/src/main/java/com/platformscience/suitabilityscore/ui/DriversFragment.kt
@@ -14,7 +14,7 @@ class DriversFragment : Fragment() {
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
+	): View {
 		
 		binding = FragmentDriversBinding.inflate(
 			inflater,
